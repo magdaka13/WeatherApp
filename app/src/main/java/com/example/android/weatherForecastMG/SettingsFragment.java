5e2cf5e35b135d8a0c3c6980911acdd327c81d28
Magdalena Grzeskoexit <magdalenagrzesko@gmail.com>
2018-03-02 13:36:04 +0100

    updated

--- app/src/main/java/com/example/android/weatherForecastMG/SettingsFragment.java
+++ app/src/main/java/com/example/android/weatherForecastMG/SettingsFragment.java
@@ -1,18 +1,3 @@
-/*
- * Copyright (C) 2016 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.example.android.weatherForecastMG;
 
 import android.app.Activity;
@@ -34,8 +19,6 @@ import com.example.android.weatherForecastMG.sync.WeatherForecastSyncUtils;
  * set their preferred weather location, and indicate whether or not they'd like to see
  * notifications.
  *
- * Please note: If you are using our dummy weather services, the location returned will always be
- * Mountain View, California.
  */
 public class SettingsFragment extends PreferenceFragmentCompat implements
         SharedPreferences.OnSharedPreferenceChangeListener {
